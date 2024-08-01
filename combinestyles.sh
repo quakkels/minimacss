26b9b8458dd2e444a10adc91de59ed3cbda41d7d
Quakkels <1100670+quakkels@users.noreply.github.com>
2024-07-31 23:55:01 -0500

    Add base grid styles

--- combinestyles.sh
+++ combinestyles.sh
@@ -1,2 +1,2 @@
 rm minima.css
-cat minima.config.css minima.reset.css minima.layout.css minima.typography.css minima.buttons.css minima.forms.css >> minima.css
+cat minima.config.css minima.reset.css minima.layout.css minima.typography.css minima.buttons.css minima.forms.css minima.grid.css >> minima.css
