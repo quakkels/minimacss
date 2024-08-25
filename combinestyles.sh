e5c5f0b4487992e5018646002c3f90d7f6ac33a0
Quakkels <1100670+quakkels@users.noreply.github.com>
2024-08-25 17:11:42 -0500

    Updated readme. Config variable naming conventions. Added block error, warn, and info styles.

--- combinestyles.sh
+++ combinestyles.sh
@@ -1,3 +1,3 @@
 touch minima.css
 rm minima.css
-cat minima.config.css minima.reset.css minima.layout.css minima.typography.css minima.buttons.css minima.forms.css minima.grid.css >> minima.css
+cat minima.config.css minima.reset.css minima.layout.css minima.typography.css minima.buttons.css minima.forms.css minima.grid.css minima.messages.css >> minima.css
