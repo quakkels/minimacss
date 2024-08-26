a89c6102ba2c6e51d992a8373998d9d100f9e4ee
Quakkels <1100670+quakkels@users.noreply.github.com>
2024-08-26 07:37:50 -0500

    combinestyles.sh will overwrite instead of append.

--- combinestyles.sh
+++ combinestyles.sh
@@ -1,3 +1,1 @@
-touch minima.css
-rm minima.css
-cat minima.config.css minima.reset.css minima.layout.css minima.typography.css minima.buttons.css minima.forms.css minima.grid.css minima.messages.css >> minima.css
+cat minima.config.css minima.reset.css minima.layout.css minima.typography.css minima.buttons.css minima.forms.css minima.grid.css minima.messages.css > minima.css
