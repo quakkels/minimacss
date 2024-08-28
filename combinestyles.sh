b29945e12811a06dd3c5fb840c5dee4901b61367
Quakkels <1100670+quakkels@users.noreply.github.com>
2024-08-28 12:43:13 -0500

    tables

--- combinestyles.sh
+++ combinestyles.sh
@@ -1,1 +1,1 @@
-cat minima.config.css minima.reset.css minima.layout.css minima.typography.css minima.buttons.css minima.forms.css minima.grid.css minima.messages.css > minima.css
+cat minima.config.css minima.reset.css minima.layout.css minima.typography.css minima.buttons.css minima.forms.css minima.grid.css minima.messages.css minima.tables.css > minima.css
